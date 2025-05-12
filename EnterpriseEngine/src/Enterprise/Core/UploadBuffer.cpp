560d7044856202b0195c27b318a53362b859e506
Peter <peter.lemanski@gmail.com>
2025-05-12 09:26:25 -0700

    Add upload buffer abstraction to help with memory allocation and resource upload to gpu

---
+++ EnterpriseEngine/src/Enterprise/Core/UploadBuffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Peter on 5/12/2025.
+//
+
+#include "UploadBuffer.h"
---
+++ EnterpriseEngine/src/Enterprise/Core/UploadBuffer.h
@@ -0,0 +1,71 @@
+//
+// Created by Peter on 5/12/2025.
+//
+
+#ifndef UPLOADBUFFER_H
+#define UPLOADBUFFER_H
+
+#include <wrl/client.h>
+#include <directx/d3d12.h>
+
+#include <memory>
+#include <deque>
+
+
+class UploadBuffer {
+public:
+    struct Allocation {
+        void * CPU;
+        D3D12_GPU_VIRTUAL_ADDRESS GPU;
+    };
+
+    explicit UploadBuffer(size_t pageSize = 2 * 1024 * 1024);
+
+    size_t GetPageSize() const { return m_PageSize; }
+
+    Allocation Allocate(size_t sizeInBytes, size_t alignment);
+
+    void Reset();
+
+private:
+    struct Page {
+        Page(size_t sizeInBytes);
+
+        bool HasSpace(size_t sizeInBytes, size_t alignment ) const;
+
+        Allocation Allocate( size_t sizeInBytes, size_t alignment );
+
+        void Reset();
+
+    private:
+        Microsoft::WRL::ComPtr<ID3D12Resource> m_D3D12Resource;
+
+        // Base pointer
+        void* m_pCPU;
+        D3D12_GPU_VIRTUAL_ADDRESS m_pGPU;
+
+        //Allocated Page size
+        size_t m_PageSize;
+
+        //Current allocation offset in bytes
+        size_t m_Offset;
+    };
+
+    using PagePool = std::deque< std::shared_ptr<Page> >;
+
+    std::shared_ptr<Page> RequestPage();
+
+    PagePool m_PagePool;
+
+    PagePool m_AvailablePages;
+
+    std::shared_ptr<Page> m_CurrentPage;
+
+    size_t m_PageSize;
+
+
+};
+
+
+
+#endif //UPLOADBUFFER_H
